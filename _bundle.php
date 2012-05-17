a5b7610f19e81745c5da49fd512eb1583ff2b73b
Kelly Becker <kellylsbkr@gmail.com>
2012-05-17 16:24:27 -0600

    environment events

--- _bundle.php
+++ _bundle.php
@@ -47,7 +47,7 @@ class Bundle {
 		/**
 		 * Load the model and return the environment
 		 */
-		$env = $this->default->model('$_environment', array('env' => 1));
+		$env = $this->default->model('_environment', array('env' => 1));
 		return $env->__toArray();
 	}
 
@@ -62,7 +62,7 @@ class Bundle {
 		/**
 		 * Load the model and return the environment
 		 */
-		$env = $this->default->model('$_environment', array('env' => 1));
+		$env = $this->default->model('_environment', array('env' => 1));
 		$env->save($array, true);
 		return $env->__toArray();
 	}
